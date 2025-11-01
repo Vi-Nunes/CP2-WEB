a16b84145c57f4b644a4a4d72749ac11b316d7bd
Victor Nunes <visubraz@gmai.com>
2025-11-01 19:54:32 -0300

    Update do header (menu de navegação), criação das páginas referentes ao header

--- OneDrive/Área de Trabalho/CP2 - web/src/js/srcipt.js
+++ OneDrive/Área de Trabalho/CP2 - web/src/js/srcipt.js
@@ -3,3 +3,5 @@ const botaoTema = document.getElementById("toggle-theme");
 botaoTema.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+document.getElementById("ano").textContent = new Date().getFullYear();
